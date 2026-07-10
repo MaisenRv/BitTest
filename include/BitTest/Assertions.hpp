9eb8dc14dafcf345463601f82ee8e4f556dd6f16
dsmr linux <dsmr19961@gmail.com>
2026-07-09 23:56:05 -0500

    fix(Assertions): enhance BIT_ASSERT and BIT_EXPECT macros for improved boolean evaluation

--- include/BitTest/Assertions.hpp
+++ include/BitTest/Assertions.hpp
@@ -14,21 +14,28 @@
 #define BIT_FAIL(expr) BitTest::engine::fail(true,#expr,__FILE__,__LINE__)
 #define BIT_PASS() BitTest::engine::pass()
 
+// ASSERT
 #define BIT_ASSERT(condition) BitTest::engine::evaluateAssert(#condition,condition,__FILE__,__LINE__)
 #define BIT_ASSERT_EQ(expected, actual) BitTest::engine::evaluateAssertEquals<decltype(expected),decltype(actual)>(expected,actual,#expected,#actual,__FILE__,__LINE__)
+#define BIT_ASSERT_TRUE(cond)  BitTest::engine::evaluateBoolean(true, cond, true, #cond, __FILE__, __LINE__)
+#define BIT_ASSERT_FALSE(cond) BitTest::engine::evaluateBoolean(false, cond, true, #cond, __FILE__, __LINE__)
 #define BIT_ASSERT_THROWS(expression) \
     try { \
         expression; \
         BitTest::engine::fail(true, #expression, __FILE__, __LINE__); \
     } catch (...) { BitTest::engine::pass(); }
 
+// EXPECT
 #define BIT_EXPECT(condition) BitTest::engine::evaluateExpect(#condition,condition,__FILE__,__LINE__)
 #define BIT_EXPECT_EQ(expected, actual) BitTest::engine::evaluateExpectEquals<decltype(expected),decltype(actual)>(expected,actual,#expected,#actual,__FILE__,__LINE__)
+#define BIT_EXPECT_TRUE(cond)  BitTest::engine::evaluateBoolean(true, cond, false, #cond, __FILE__, __LINE__)
+#define BIT_EXPECT_FALSE(cond) BitTest::engine::evaluateBoolean(false, cond, false, #cond, __FILE__, __LINE__)
 #define BIT_EXPECT_THROWS(expression) \
     try { \
         expression; \
         BitTest::engine::fail(false, #expression, __FILE__, __LINE__); \
     } catch (...) { BitTest::engine::pass(); }
+
 
 namespace BitTest::engine{
     inline void fail(bool isFatal, const char *expr,const char *fileName, int line){
@@ -45,6 +52,16 @@ namespace BitTest::engine{
         std::cout << GREEN << "\t[TEST PASSED]" << RESET << std::endl;
     }
 
+    inline void evaluateBoolean(bool expected, bool actual, bool isFatal, const char* strResult, const char *fileName, int line) {
+        if (actual == expected) {
+            BitTest::engine::pass();
+            return;
+        }
+        std::stringstream ss;
+        ss << "Expected [" << strResult << "] to be " << (expected ? "TRUE":"FALSE" ) << ", but it evaluated to " << (actual ? "TRUE":"FALSE" );
+        BitTest::engine::fail(isFatal, ss.str().c_str(), fileName, line);
+    }
+
     inline void evaluateAssert(const char *expr, bool conditionResult, const char *fileName, int line){
         if(conditionResult){
             BitTest::engine::pass();
@@ -54,9 +71,13 @@ namespace BitTest::engine{
     }
     template <typename T1, typename T2>
     inline void evaluateAssertEquals(T1 expected, T2 actual, const char* strExpected, const char* strActual, const char *fileName, int line){
+        if (actual == expected) {
+            BitTest::engine::pass();
+            return;
+        }
         std::stringstream ss;
         ss << strExpected << " == " << strActual << "| Expected " << expected << ", it was obtained " << actual;
-        BitTest::engine::evaluateAssert(ss.str().c_str(), expected == actual, fileName, line );
+        BitTest::engine::fail(true, ss.str().c_str(), fileName, line);
     }
     
     inline void evaluateExpect(const char *expr, bool conditionResult, const char *fileName, int line){
@@ -69,8 +90,12 @@ namespace BitTest::engine{
 
     template <typename T1, typename T2>
     inline void evaluateExpectEquals(T1 expected, T2 actual, const char* strExpected, const char* strActual, const char *fileName, int line){
+        if (actual == expected) {
+            BitTest::engine::pass();
+            return;
+        }
         std::stringstream ss;
         ss << strExpected << " == " << strActual << "| Expected " << expected << ", it was obtained " << actual;
-        BitTest::engine::evaluateExpect(ss.str().c_str(), expected == actual, fileName, line );
+        BitTest::engine::fail(false, ss.str().c_str(), fileName, line);
     }
 }
